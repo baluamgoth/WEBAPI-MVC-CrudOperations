25473584111c36da483f93a6d3b8cdb7e2cde78e
baluamgoth <baluamgoth@gmail.com>
2020-08-30 17:20:10 +0530

    Added WEB API controller(EmpCrud) and added action mentions and returning all details from database

---
+++ WEBAPI-MVC-Crud/Controllers/EmpCrudController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WEBAPI_MVC_Crud.Models;
+
+namespace WEBAPI_MVC_Crud.Controllers
+{
+    public class EmpCrudController : ApiController
+    {
+        public IHttpActionResult getemp()
+        {
+            cmEntities cm = new cmEntities();
+            var results = cm.Newempregs.ToList();
+            return Ok(results);
+        }
+    }
+}
