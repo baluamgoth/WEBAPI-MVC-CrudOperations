7c5a409000abedaf0deec06d747d2a1b809c813c
baluamgoth <baluamgoth@gmail.com>
2020-08-30 17:03:00 +0530

    Added Model EmpClass in project

---
+++ WEBAPI-MVC-Crud/Models/EmpClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBAPI_MVC_Crud.Models
+{
+    public class EmpClass
+    {
+        public int Empid { get; set; }
+        public string Empname { get; set; }
+        public string Empemail { get; set; }
+        public string Emplocation { get; set; }
+        public string EmpDesignation { get; set; }
+    }
+}
